fce67dd08a0963550c49b65f03f650bb5cd6283b
Timothy LENGLINE <tlenglin@student.42.fr>
2018-01-12 17:05:15 +0100

    main created, flags added

---
+++ ID_parser.py
@@ -0,0 +1,24 @@
+import argparse
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-f", "--file", required=True, help="path to the ID card")
+ap.add_argument("-s", "--step", help="step you want to execute : 1 for rotate, 2 for crop the mrz, 3 for the OCR, 4 for parse the mrz")
+args = vars(ap.parse_args())
+
+import_list = {}
+import_list["1"] = "rotation_spacing"
+import_list["2"] = "MRZ_crop"
+import_list["3"] = "OCR_to_text"
+import_list["4"] = "MRZ_parsing"
+
+filename = args["file"]
+image = cv2.imread(filename)
+cv2.imwrite("step0.jpg", image)
+
+if args["step"] != None:
+    steps = args["step"]
+    for i in range(len(steps)):
+        module = import_list[steps[i]]
+        command = __import__(module)
+        command.run()
