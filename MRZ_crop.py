8956804bd425fdc48e12870dda657bfac17dd89c
Timothy LENGLINE <tlenglin@student.42.fr>
2018-01-13 12:06:23 +0100

    no more arg, search for step file

--- MRZ_crop.py
+++ MRZ_crop.py
@@ -1,15 +1,9 @@
 # import the necessary packages
 from imutils import paths
 import numpy as np
-import argparse
 import imutils
 import cv2
 from PIL import Image
-
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--images", required=True, help="path to images directory")
-args = vars(ap.parse_args())
 
 # initialize a rectangular and square structuring kernel
 rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (13, 5))
@@ -95,5 +89,5 @@ for c in cnts:
 
 #save MRZ images
 #roi.save("step2.jpg")
-roi = cv2.threshold(roi, 82, 255, cv2.THRESH_BINARY)[1]
+roi = cv2.threshold(roi, 100, 255, cv2.THRESH_BINARY)[1]
 cv2.imwrite("step2.jpg", roi)
