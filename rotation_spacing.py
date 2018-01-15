bc49802da7d3e8c123d10497514628ec058eafba
Timothy LENGLINE <tlenglin@student.42.fr>
2018-01-15 19:16:27 +0100

    deletion of comments

--- rotation_spacing.py
+++ rotation_spacing.py
@@ -17,8 +17,6 @@ import matplotlib.pyplot as plt
 from matplotlib.mlab import rms_flat
 from imutils import paths
 try:
-    # More accurate peak finding from
-    # https://gist.github.com/endolith/255291#file-parabolic-py
     from parabolic import parabolic
 
     def argmax(x):
@@ -30,19 +28,14 @@ except ImportError:
 filename = "step0.jpg"
 img = Image.open(filename)
 
-# Load file, converting to grayscale
 I = asarray(Image.open(filename).convert('L'))
-I = I - mean(I)  # Demean; make the brightness extend above and below zero
+I = I - mean(I)
 
 sinogram = radon(I)
 
-# Find the RMS value of each row and find "busiest" rotation,
-# where the transform is lined up perfectly with the alternating dark
-# text and white lines
 r = array([rms_flat(line) for line in sinogram.transpose()])
 rotation = argmax(r)
 print('Rotation: {:.2f} degrees'.format(90 - rotation))
 
 img2 = img.rotate(90 - rotation, expand=True)
-img2.convert('L').save("./step1.jpg") #save a grayscale version of the image ?
-#save it in a "rotated_images" directory ?
+img2.convert('L').save("./step1.jpg")
