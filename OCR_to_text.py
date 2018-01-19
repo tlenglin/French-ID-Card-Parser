10b2aad8fa22b1a510c1827d1a10be10f3cef449
Timothy LENGLINE <tlenglin@student.42.fr>
2018-01-19 18:00:51 +0100

    line_fixer divised, reversed_file option aborted, more scoring steps, score threshold modified

--- OCR_to_text.py
+++ OCR_to_text.py
@@ -31,21 +31,25 @@ def delete_spaces(line):
 letters = {'0': 'O', '1': 'I', '2': 'Z', '3': 'B', '4': 'A', '5': 'S', '6': 'G', '7': 'T', '8': 'B' }
 numbers = {'B': '8', 'C': '0', 'D': '0', 'G': '6', 'I': '1', 'O': '0', 'Q': '0', 'S': '5', 'Z': '2'}
 
-def line_fixer(line1, line2):
+def line_fixer1(line1):
     line1 = line1.upper()
-    line2 = line2.upper()
 
     for i in range(len(line1)):
         if i < 30 and letters.get(line1[i]) != None:
             line1 = line1[:30].replace(line1[i], letters[line1[i]]) + line1[30:]
         elif i > 29 and numbers.get(line1[i]) != None:
             line1 = line1[:30] + line1[30:].replace(line1[i], numbers[line1[i]])
+    return line1
+
+def line_fixer2(line2):
+    line2 = line2.upper()
+
     for i in range(len(line2)):
         if ((i > 12 and i < 27)) and letters.get(line2[i]) != None:
             line2 = line2[:13] + line2[13:27].replace(line2[i], letters[line2[i]]) + line2[27:]
         elif (i < 13 or (i > 26 and i < 34)) and numbers.get(line2[i]) != None:
             line2 = line2[:13].replace(line2[i], numbers[line2[i]]) + line2[13:27] + line2[27:34].replace(line2[i], numbers[line2[i]]) + line2[34:]
-    return [line1, line2]
+    return line2
 
 def fill_file(mrz, filename):
     file = open(filename, "w")
@@ -64,6 +68,7 @@ def empty_line(line): #si ligne pas complete, ou trop longue ?
     return 1
 
 def reversed_line(line):
+    print line
     for i in range(len(line)):
         if line[i] == '>':
             return 0
@@ -170,10 +175,10 @@ def align_line1(aligned_MRZ, line1):
             i = 29 + len(line1) - i1_end - 1
             i1_end = len(line1) - 1
         while (i1_end >= 0 and i >= 0):
-            print "i = ", i
-            print "i1_end", i1_end
-            print "len(line1) = ", len(line1)
-            print "len(aligned_MRZ) = ", len(aligned_MRZ)
+            # print "i = ", i
+            # print "i1_end", i1_end
+            # print "len(line1) = ", len(line1)
+            # print "len(aligned_MRZ) = ", len(aligned_MRZ)
             if ((i == len(aligned_MRZ) - 1) and (score1(aligned_MRZ[:i] + line1[i1_end]) >= score1(aligned_MRZ))): #atenttion premiere somme avec i = len
                 aligned_MRZ = aligned_MRZ[:i] + line1[i1_end]
             elif i != len(aligned_MRZ) and score1(aligned_MRZ[:i] + line1[i1_end] + aligned_MRZ[i + 1:]) >= score1(aligned_MRZ):
@@ -265,13 +270,14 @@ def ocr():
 
     i = 0
     lines = {}
+    cleaned_MRZ = ['', '']
     for line in file:
         if line is not None and empty_line(line) == 0:
             i = i + 1
-            if reversed_line(line) == 0:
-                #revert_file()
-                print "error : file reverted"
-                sys.exit
+            # if reversed_line(line) == 0:
+            #     #revert_file()
+            #     print "error : file reverted"
+            #     sys.exit
             line = delete_spaces(line)
             lines[str(i)] = line
     if (i > 2):
@@ -280,7 +286,10 @@ def ocr():
 
     print "test sortie de if"
     print lines
-    cleaned_MRZ = line_fixer(lines["1"], lines["2"])
+    if len(lines) > 0:
+        cleaned_MRZ[0] = line_fixer1(lines["1"])
+    if len(lines) > 1:
+        cleaned_MRZ[1] = line_fixer2(lines["2"])
     return cleaned_MRZ
 
 
@@ -293,34 +302,39 @@ aligned_MRZ = align_lines(aligned_MRZ, cleaned_MRZ[0], cleaned_MRZ[1])
 s0 = score(aligned_MRZ[0], aligned_MRZ[1])
 print "s0 = ", s0
 
-if s0 < 90:
+if s0 < 88:
     print "s0 < 80"
     from MRZ_crop import MRZ_crop
     MRZ_crop(90)
     new_MRZ = ocr()
     print "passage du 2eme ocr()"
     aligned_MRZ = align_lines(aligned_MRZ, new_MRZ[0], new_MRZ[1])
-    MRZ_crop(160)
-    new_MRZ = ocr()
-    print "passage du 3eme ocr"
-    aligned_MRZ = align_lines(aligned_MRZ, new_MRZ[0], new_MRZ[1])
     s1 = score(aligned_MRZ[0], aligned_MRZ[1])
     print "s1 = ", s1
-    if s1 < 90:
-        print "s1 < 80"
-        MRZ_crop(60)
+    if s1 < 88:
+        MRZ_crop(160)
         new_MRZ = ocr()
-        aligned_MRZ = align_lines(aligned_MRZ, new_MRZ[0], new_MRZ[1])
-        MRZ_crop(190)
-        new_MRZ = ocr()
+        print "passage du 3eme ocr"
         aligned_MRZ = align_lines(aligned_MRZ, new_MRZ[0], new_MRZ[1])
         s2 = score(aligned_MRZ[0], aligned_MRZ[1])
         print "s2 = ", s2
-        if s2 < 90:
-            if s2 > 50:
-                print "uncertain result, 50 < s2 < 80"
-            else:
-                print "probably no result s2 < 50"
+        if s2 < 88:
+            print "s2 < 80"
+            MRZ_crop(60)
+            new_MRZ = ocr()
+            aligned_MRZ = align_lines(aligned_MRZ, new_MRZ[0], new_MRZ[1])
+            s3 = score(aligned_MRZ[0], aligned_MRZ[1])
+            if s3 < 88:
+                MRZ_crop(190)
+                new_MRZ = ocr()
+                aligned_MRZ = align_lines(aligned_MRZ, new_MRZ[0], new_MRZ[1])
+                s4 = score(aligned_MRZ[0], aligned_MRZ[1])
+                print "s4 = ", s4
+                if s4 < 88:
+                    if s4 > 50:
+                        print "uncertain result, 50 < s4 < 80"
+                    else:
+                        print "probably no result s4 < 50"
 
 print ("creation of step4.txt")
 fill_file(aligned_MRZ, "step4.txt")
